a47927289a5865b8764f2ebbc67258ec5f75b72f
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-20 16:36:42 +0000

    added test functions, but errors on seeWin2

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,9 +14,85 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-/* EXAMPLE TEST FORMAT
-TEST(TicTacToeBoardTest, sanityCheck)
+TEST(TicTacToeBoardTest, willToggle)
 {
-	ASSERT_TRUE(true);
+	TicTacToeBoard test;
+	Piece asserted = O;
+	Piece toggledTo = test.toggleTurn();
+	EXPECT_TRUE(toggledTo == asserted);
 }
-*/
+
+TEST(TicTacToeBoardTest, canPlace1)
+{
+	TicTacToeBoard test;
+	Piece placed = test.placePiece(0,0);
+	EXPECT_TRUE(placed == test.getPiece(0,0));
+}
+
+TEST(TicTacToeBoardTest, cannotPlaceColumn)
+{
+	TicTacToeBoard test;
+	Piece asserted = Invalid;
+	Piece placed = test.placePiece(0,3);
+	EXPECT_TRUE(placed == asserted);
+}
+
+TEST(TicTacToeBoardTest, cannotPlaceRow)
+{
+	TicTacToeBoard test;
+	Piece asserted = Invalid;
+	Piece placed = test.placePiece(3,0);
+	EXPECT_TRUE(placed == asserted);
+}	
+
+TEST(TicTacToeBoardTest, cannotPlaceNegativeColumn)
+{	
+	TicTacToeBoard test;
+	Piece asserted = Invalid;
+	Piece placed = test.placePiece(0, -1);
+	EXPECT_TRUE(placed == asserted);
+}
+
+TEST(TicTacToeBoardTest, cannotPlaceNegativeRow)
+{	
+	TicTacToeBoard test;
+	Piece asserted = Invalid;
+	Piece placed = test.placePiece(-1, 0);
+	EXPECT_TRUE(placed == asserted);
+}
+
+// This test will fail due to the board not being filled even though X has won My 'bug'
+TEST(TicTacToeBoardTest, seeWin)
+{
+	TicTacToeBoard test;
+	Piece asserted = X;
+	test.placePiece(0, 0); //place X
+	test.placePiece(0, 1);
+	test.placePiece(1, 0); //place X
+	test.placePiece(2, 2);
+	test.placePiece(2, 0); //place X
+	EXPECT_TRUE(test.getWinner() == asserted);
+}
+
+TEST(TicTacToeBoardTest, seeWin2)
+{
+	TicTacToeBoard test;
+	Piece asserted = Blank;
+	for(int i=0; i<2; i++)
+		for(int j=0; j<BOARDSIZE; j++)
+			test.placePiece(i, j);
+	test.placePiece(2,1);
+	test.placePiece(2,0);
+	test.placePiece(2,2);
+	EXPECT_TRUE(test.getWinner() == asserted);
+}
+
+TEST(TicTacToeBoardTest, seeWin3)
+{
+	TicTacToeBoard test;
+	Piece asserted = X;
+	for(int i=0; i<BOARDSIZE; i++)
+		for(int j=0; j<BOARDSIZE; j++)
+			test.placePiece(i, j);
+	EXPECT_TRUE(test.getWinner() == asserted);
+}
